935d51ac50fea782238be6dfe896a49b94f320bf
Henrik Eriksson <henrik.eriksson.android@gmail.com>
2018-03-04 13:27:58 +0100

    Error with healthcheck

--- src/main/java/se/henrikeriksson/greenhouse/health/RemoteHealth.java
+++ src/main/java/se/henrikeriksson/greenhouse/health/RemoteHealth.java
@@ -6,7 +6,7 @@ import se.henrikeriksson.greenhouse.api.URLHelper;
 public class RemoteHealth extends HealthCheck {
     @Override
     protected Result check() throws Exception {
-        return URLHelper.ping("wiktoreriksson.se",1) ?
+        return URLHelper.ping("192.168.0.103",1) ?
                 Result.healthy("Wiktors PI accessible"):
                 Result.unhealthy("Wiktors PI down");
     }
