997bcd680ed2e956855da3a200a3ed73b67869e1
Henrik Eriksson <henrik.eriksson.android@gmail.com>
2018-03-11 07:35:16 +0100

    Add shebang "#!/usr/bin/env bash"

--- res/sh/sftpjar.sh
+++ res/sh/sftpjar.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 /usr/bin/sftp pi@192.168.0.112:/home/pi <<< $'put /Users/henrikeriksson/Development/gitrepos/greenhouse/target/greenhouse-1.0-SNAPSHOT-shaded.jar /home/pi/greenhouse/greenhouse.jar'
