c19466a0f74bb25c47440100eb14399e167bbc45
Henrik Eriksson <henrik.eriksson.android@gmail.com>
2018-06-22 18:02:20 +0200

    Mail #3

--- src/main/java/se/henrikeriksson/greenhouse/utils/MailHelper.java
+++ src/main/java/se/henrikeriksson/greenhouse/utils/MailHelper.java
@@ -1,15 +1,23 @@
 package se.henrikeriksson.greenhouse.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 public class MailHelper {
+    private final static Logger log = LoggerFactory.getLogger(MailHelper.class);
+
     private static boolean maillow = true;
     private static boolean mailhigh = false;
+    private static String emailReceiver = "henrik.eriksson.android@gmail.com";
+
     public static void waterlowmail() {
         if (maillow) {
+            log.info(String.format("Water is low, send email to: %s", emailReceiver));
             Runtime r = Runtime.getRuntime();
             try {
-                r.exec("echo -e \"Subject: Tank\n\nALERT: Water low in tank. You will get a mail when you fix the water tank.\" | sendmail -v henrik.eriksson.android@gmail.com");
+                r.exec(String.format("echo -e \"Subject: Tank\n\nALERT: Water low in tank. You will get a mail when you fix the water tank.\" | sendmail -v %s", emailReceiver));
             } catch (IOException e) {
                 e.printStackTrace();
                 throw new RuntimeException(e);
@@ -20,9 +28,10 @@ public class MailHelper {
     }
     public static void waterhighmail() {
         if (mailhigh) {
+            log.info(String.format("Filled up water tank!, send email to: %s", emailReceiver));
             Runtime r = Runtime.getRuntime();
             try {
-                r.exec("echo -e \"Subject: Tank\n\nFilled up water tank!\" | sendmail -v henrik.eriksson.android@gmail.com");
+                r.exec(String.format("echo -e \"Subject: Tank\\n\\nFilled up water tank!\" | sendmail -v %s", emailReceiver));
             } catch (IOException e) {
                 e.printStackTrace();
                 throw new RuntimeException(e);
